2dccb0466c300fecaf2c1489a7e92c57c27205cc
María <72228855+MariaRod123@users.noreply.github.com>
2023-02-16 10:05:07 -0300

    Add files via upload

---
+++ js/colores_lista.js
@@ -0,0 +1,41 @@
+let list = document.getElementById('list');
+let par = document.getElementById('par');
+let impar = document.getElementById('impar');
+
+let colorActual = "lightgray";
+// Manejar el clic en el botón "Colorear pares"
+par.addEventListener("click", function() {
+    // Obtener todos los elementos de la lista
+    let elementos = list.getElementsByTagName("li");
+    
+    // Recorrer los elementos de la lista y cambiar el color de los pares
+    for (let i = 0; i < elementos.length; i += 2) {
+      elementos[i].style.backgroundColor = colorActual;
+    }
+
+    
+    // Cambiar el color actual para la próxima vez
+    if (colorActual === "lightgray") {
+      colorActual = "white";
+    } else {
+      colorActual = "lightgray";
+    }
+  });
+  
+  // Manejar el clic en el botón "Colorear impares"
+  impar.addEventListener("click", function() {
+    // Obtener todos los elementos de la lista
+    let elementos = list.getElementsByTagName("li");
+    
+    // Recorrer los elementos de la lista y cambiar el color de los impares
+    for (let i = 1; i < elementos.length; i += 2) {
+      elementos[i].style.backgroundColor = colorActual;
+    }
+
+    // Cambiar el color actual para la próxima vez
+    if (colorActual === "lightgray") {
+      colorActual = "white";
+    } else {
+      colorActual = "lightgray";
+    }
+  });

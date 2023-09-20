38be17dfc0596be999bd2ced8d910e4f5a2d1c99
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-09-19 21:22:12 -0300

    Update colores_lista.js

--- colores_lista.js
+++ colores_lista.js
@@ -1,13 +1,13 @@
 /*cambiar los colores de la fuente de los elementos pares e impares de una lista al clickear un botón*/
 
-let lista = document.getElementById('list');
-let par = document.getElementById('par');
-let impar = document.getElementById('impar');
+const lista = document.getElementById('list');
+const par = document.getElementById('par');
+const impar = document.getElementById('impar');
 
 let colorActual = "black";
 
 par.addEventListener("click", function() {
-    let elementosPares = lista.querySelectorAll('li:nth-child(even)');
+    const elementosPares = lista.querySelectorAll('li:nth-child(even)');
     
     for (const element of elementosPares) {
       element.style.color = colorActual;
@@ -21,7 +21,7 @@ par.addEventListener("click", function() {
 });
 
 impar.addEventListener("click", function() {
-    let elementosImpares = lista.querySelectorAll('li:nth-child(odd)');
+    const elementosImpares = lista.querySelectorAll('li:nth-child(odd)');
     
     for (const element of elementosImpares) {
       element.style.color = colorActual;
